c405dd9fd7cdf37c5503970abb1389283cd9dcec
XieBiao <hhxsv5@sina.com>
2019-01-23 22:52:36 +0800

    add websocket messages

--- websocket.go
+++ websocket.go
@@ -1,6 +1,9 @@
 package kucoin
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type WebSocketTokenModel struct {
 	Token   string                `json:"token"`
@@ -24,4 +27,56 @@ func (as *ApiService) WebSocketPublicToken() (*ApiResponse, error) {
 func (as *ApiService) WebSocketPrivateToken() (*ApiResponse, error) {
 	req := NewRequest(http.MethodPost, "/api/v1/bullet-private", map[string]string{})
 	return as.call(req)
+}
+
+const (
+	WelcomeMessage     = "welcome"
+	PingMessage        = "ping"
+	PongMessage        = "pong"
+	SubscribeMessage   = "subscribe"
+	AckMessage         = "ack"
+	UnsubscribeMessage = "unsubscribe"
+)
+
+type WebSocketMessage struct {
+	Id   string `json:"id"`
+	Type string `json:"type"`
+}
+
+type WebSocketSubscribeMessage struct {
+	*WebSocketMessage
+	Topic          string `json:"topic"`
+	PrivateChannel bool   `json:"privateChannel"`
+	Response       bool   `json:"response"`
+}
+
+func NewPingMessage() *WebSocketMessage {
+	return &WebSocketMessage{
+		Id:   IntToString(time.Now().UnixNano()),
+		Type: PingMessage,
+	}
+}
+
+func NewSubscribeMessage(topic string, privateChannel, response bool) *WebSocketSubscribeMessage {
+	return &WebSocketSubscribeMessage{
+		WebSocketMessage: &WebSocketMessage{
+			Id:   IntToString(time.Now().UnixNano()),
+			Type: SubscribeMessage,
+		},
+		Topic:          topic,
+		PrivateChannel: privateChannel,
+		Response:       response,
+	}
+}
+
+func NewUnsubscribeMessage(topic string, privateChannel, response bool) *WebSocketSubscribeMessage {
+	return &WebSocketSubscribeMessage{
+		WebSocketMessage: &WebSocketMessage{
+			Id:   IntToString(time.Now().UnixNano()),
+			Type: UnsubscribeMessage,
+		},
+		Topic:          topic,
+		PrivateChannel: privateChannel,
+		Response:       response,
+	}
 }
