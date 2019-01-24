70ba35b053b06bd3b9276e5def4bd2115e9c3903
XieBiao <hhxsv5@sina.com>
2019-01-24 20:32:50 +0800

    update examples

--- examples/websocket.go
+++ examples/websocket.go
@@ -16,10 +16,25 @@ func main() {
 
 	mc, done, ec := s.WebSocketSubscribePublicChannel("/market/ticker:KCS-BTC", true)
 	var i = 0
+	type Ticker struct {
+		Sequence    string `json:"sequence"`
+		BestAsk     string `json:"bestAsk"`
+		Size        string `json:"size"`
+		BestBidSize string `json:"bestBidSize"`
+		Price       string `json:"price"`
+		BestAskSize string `json:"bestAskSize"`
+		BestBid     string `json:"bestBid"`
+	}
 	for {
 		select {
 		case m := <-mc:
-			log.Printf("Received: %s", kucoin.ToJsonString(m))
+			// log.Printf("Received: %s", kucoin.ToJsonString(m))
+			t := &Ticker{}
+			if err := m.ReadData(t); err != nil {
+				log.Printf("Failure to read: %s", err.Error())
+				return
+			}
+			log.Printf("Ticker: %s, %s, %s", t.Sequence, t.Price, t.Size)
 			i++
 			if i == 3 {
 				log.Println("Exit subscription")
