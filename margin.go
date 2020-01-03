6aca4d78dd239e697c6ab6bc8b4f800dcce483f3
hugo <hugo.hu@kucoin.com>
2020-01-03 11:53:11 +0800

    margin lend borrow

--- margin.go
+++ margin.go
@@ -52,3 +52,282 @@ func (as *ApiService) MarginAccount() (*ApiResponse, error) {
 	req := NewRequest(http.MethodGet, "/api/v1/margin/account", nil)
 	return as.Call(req)
 }
+
+//发布借入委托
+type CreateBorrowOrderResultModel struct {
+	OrderId  string `json:"orderId"`
+	Currency string `json:"currency"`
+}
+
+func (as *ApiService) CreateBorrowOrder(params map[string]string) (*ApiResponse, error) {
+	req := NewRequest(http.MethodPost, "/api/v1/margin/borrow", params)
+	return as.Call(req)
+}
+
+//查询借入委托
+type BorrowOrderModel struct {
+	OrderId   string      `json:"orderId"`
+	Currency  string      `json:"currency"`
+	Size      json.Number `json:"size"`
+	Filled    json.Number `json:"filled"`
+	Status    string      `json:"status"`
+	MatchList []struct {
+		Currency     string      `json:"currency"`
+		DailyIntRate json.Number `json:"dailyIntRate"`
+		Size         json.Number `json:"size"`
+		Term         json.Number `json:"term"`
+		Timestamp    json.Number `json:"timestamp"`
+		TradeId      string      `json:"tradeId"`
+	} `json:"matchList"`
+}
+
+func (as *ApiService) BorrowOrder(orderId string) (*ApiResponse, error) {
+	params := map[string]string{}
+	if orderId != "" {
+		params["orderId"] = orderId
+	}
+	req := NewRequest(http.MethodGet, "/api/v1/margin/borrow", params)
+	return as.Call(req)
+}
+
+//查询待还款记录
+type BorrowOutstandingRecordModel struct {
+	Currency        string      `json:"currency"`
+	TradeId         string      `json:"tradeId"`
+	Liability       json.Number `json:"liability"`
+	Principal       json.Number `json:"principal"`
+	AccruedInterest json.Number `json:"accruedInterest"`
+	CreatedAt       json.Number `json:"createdAt"`
+	MaturityTime    json.Number `json:"maturityTime"`
+	Term            json.Number `json:"term"`
+	RepaidSize      json.Number `json:"repaidSize"`
+	DailyIntRate    json.Number `json:"dailyIntRate"`
+}
+
+type BorrowOutstandingRecordsModel []*BorrowOutstandingRecordModel
+
+func (as *ApiService) BorrowOutstandingRecords(currency string, pagination *PaginationParam) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+	pagination.ReadParam(params)
+	req := NewRequest(http.MethodGet, "/api/v1/margin/borrow/outstanding", params)
+	return as.Call(req)
+}
+
+//查询已还款记录
+type BorrowRepaidRecordModel struct {
+	Currency     string      `json:"currency"`
+	DailyIntRate json.Number `json:"dailyIntRate"`
+	Interest     json.Number `json:"interest"`
+	Principal    json.Number `json:"principal"`
+	RepaidSize   json.Number `json:"repaidSize"`
+	RepayTime    json.Number `json:"repayTime"`
+	Term         json.Number `json:"term"`
+	TradeId      string      `json:"tradeId"`
+}
+type BorrowRepaidRecordsModel []*BorrowRepaidRecordModel
+
+func (as *ApiService) BorrowRepaidRecords(currency string, pagination *PaginationParam) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+	pagination.ReadParam(params)
+	req := NewRequest(http.MethodGet, "/api/v1/margin/borrow/repaid", params)
+	return as.Call(req)
+}
+
+//一键还款
+func (as *ApiService) RepayAll(params map[string]string) (*ApiResponse, error) {
+	req := NewRequest(http.MethodPost, "/api/v1/margin/repay/all", params)
+	return as.Call(req)
+}
+
+//一键还款单笔
+func (as *ApiService) RepaySingle(params map[string]string) (*ApiResponse, error) {
+	req := NewRequest(http.MethodPost, "/api/v1/margin/repay/single", params)
+	return as.Call(req)
+}
+
+//发布借出委托
+type CreateLendOrderResultModel struct {
+	OrderId string `json:"orderId"`
+}
+
+func (as *ApiService) CreateLendOrder(params map[string]string) (*ApiResponse, error) {
+	req := NewRequest(http.MethodPost, "/api/v1/margin/lend", params)
+	return as.Call(req)
+}
+
+//撤销借出委托
+func (as *ApiService) CancelLendOrder(orderId string) (*ApiResponse, error) {
+	req := NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/margin/lend/%s", orderId), nil)
+	return as.Call(req)
+}
+
+//设置自动借出
+func (as *ApiService) ToggleAutoLend(params map[string]string) (*ApiResponse, error) {
+	req := NewRequest(http.MethodPost, "/api/v1/margin/toggle-auto-lend", params)
+	return as.Call(req)
+}
+
+//查询活跃借出委托
+type LendOrderBaseModel struct {
+	OrderId      string      `json:"orderId"`
+	Currency     string      `json:"currency"`
+	Size         json.Number `json:"size"`
+	FilledSize   json.Number `json:"filledSize"`
+	DailyIntRate json.Number `json:"dailyIntRate"`
+	Term         json.Number `json:"term"`
+	CreatedAt    json.Number `json:"createdAt"`
+}
+
+type LendActiveOrderModel struct {
+	LendOrderBaseModel
+}
+
+type LendActiveOrdersModel []*LendActiveOrderModel
+
+func (as *ApiService) LendActiveOrders(currency string, pagination *PaginationParam) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+
+	pagination.ReadParam(params)
+	req := NewRequest(http.MethodGet, "/api/v1/margin/lend/active", params)
+
+	return as.Call(req)
+}
+
+//查询历史借出委托
+type LendDoneOrderModel struct {
+	LendOrderBaseModel
+	Status string `json:"status"`
+}
+
+type LendDoneOrdersModel []*LendDoneOrderModel
+
+func (as *ApiService) LendDoneOrders(currency string, pagination *PaginationParam) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+
+	pagination.ReadParam(params)
+	req := NewRequest(http.MethodGet, "/api/v1/margin/lend/done", params)
+
+	return as.Call(req)
+}
+
+//查询未结算出借记录
+type LendTradeUnsettledRecordModel struct {
+	TradeId         string      `json:"tradeId"`
+	Currency        string      `json:"currency"`
+	Size            json.Number `json:"size"`
+	AccruedInterest json.Number `json:"accruedInterest"`
+	Repaid          json.Number `json:"repaid"`
+	DailyIntRate    json.Number `json:"dailyIntRate"`
+	Term            json.Number `json:"term"`
+	MaturityTime    json.Number `json:"maturityTime"`
+}
+
+type LendTradeUnsettledRecordsModel []*LendTradeUnsettledRecordModel
+
+func (as *ApiService) LendTradeUnsettledRecords(currency string, pagination *PaginationParam) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+
+	pagination.ReadParam(params)
+	req := NewRequest(http.MethodGet, "/api/v1/margin/lend/trade/unsettled", params)
+	return as.Call(req)
+}
+
+//查询已结算出借记录
+type LendTradeSettledRecordModel struct {
+	TradeId      string      `json:"tradeId"`
+	Currency     string      `json:"currency"`
+	Size         json.Number `json:"size"`
+	Interest     json.Number `json:"interest"`
+	Repaid       json.Number `json:"repaid"`
+	DailyIntRate json.Number `json:"dailyIntRate"`
+	Term         json.Number `json:"term"`
+	SettledAt    json.Number `json:"settledAt"`
+	Note         json.Number `json:"note"`
+}
+
+type LendTradeSettledRecordsModel []*LendTradeSettledRecordModel
+
+func (as *ApiService) LendTradeSettledRecords(currency string, pagination *PaginationParam) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+
+	pagination.ReadParam(params)
+	req := NewRequest(http.MethodGet, "/api/v1/margin/lend/trade/settled", params)
+
+	return as.Call(req)
+}
+
+//资产借出记录
+type LendAssetModel struct {
+	Currency        string      `json:"currency"`
+	Outstanding     json.Number `json:"outstanding"`
+	FilledSize      json.Number `json:"filledSize"`
+	AccruedInterest json.Number `json:"accruedInterest"`
+	RealizedProfit  json.Number `json:"realizedProfit"`
+	IsAutoLend      bool        `json:"isAutoLend"`
+}
+
+type LendAssetsModel []*LendAssetModel
+
+func (as *ApiService) LendAssets(currency string) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+
+	req := NewRequest(http.MethodGet, "/api/v1/margin/lend/assets", params)
+	return as.Call(req)
+}
+
+//借出市场信息
+type MarginMarketModel struct {
+	DailyIntRate json.Number `json:"dailyIntRate"`
+	Term         json.Number `json:"term"`
+	Size         json.Number `json:"size"`
+}
+
+type MarginMarketsModel []*MarginMarketModel
+
+func (as *ApiService) MarginMarkets(params map[string]string) (*ApiResponse, error) {
+	req := NewRequest(http.MethodGet, "/api/v1/margin/market", params)
+	return as.Call(req)
+}
+
+//借贷市场成交信息
+type MarginTradeModel struct {
+	TradeId      string      `json:"tradeId"`
+	Currency     string      `json:"currency"`
+	Size         json.Number `json:"size"`
+	DailyIntRate json.Number `json:"dailyIntRate"`
+	Term         json.Number `json:"term"`
+	Timestamp    json.Number `json:"timestamp"`
+}
+
+type MarginTradesModel []*MarginTradeModel
+
+func (as *ApiService) MarginTradeLast(currency string) (*ApiResponse, error) {
+	params := map[string]string{}
+	if currency != "" {
+		params["currency"] = currency
+	}
+
+	req := NewRequest(http.MethodGet, "/api/v1/margin/trade/last", params)
+	return as.Call(req)
+}
--- margin_test.go
+++ margin_test.go
@@ -71,3 +71,488 @@ func TestApiService_MarginAccount(t *testing.T) {
 		t.Error("empty key 'Accounts'")
 	}
 }
+
+func TestApiService_CreateBorrowOrder(t *testing.T) {
+	t.SkipNow()
+	s := NewApiServiceFromEnv()
+
+	params := map[string]string{"currency": "BTC", "type": "IOC", "size": "0.012"}
+	rsp, err := s.CreateBorrowOrder(params)
+
+	if err != err {
+		t.Fatal(err)
+	}
+
+	o := &CreateBorrowOrderResultModel{}
+	if err := rsp.ReadData(o); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(o))
+
+	switch {
+	case o.OrderId == "":
+		t.Error("empty key 'OrderId'")
+	}
+}
+
+func TestApiService_BorrowOrder(t *testing.T) {
+	t.SkipNow()
+	s := NewApiServiceFromEnv()
+	rsp, err := s.BorrowOrder("5e0da5708c544f00082dcbab")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	o := &BorrowOrderModel{}
+	if err := rsp.ReadData(o); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(o))
+
+	switch {
+	case o.OrderId == "":
+		t.Error("empty key 'OrderId'")
+	}
+}
+
+func TestApiService_BorrowOutstandingRecords(t *testing.T) {
+
+	s := NewApiServiceFromEnv()
+	pagination := &PaginationParam{
+		CurrentPage: 1,
+		PageSize:    10,
+	}
+
+	rsp, err := s.BorrowOutstandingRecords("", pagination)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := BorrowOutstandingRecordsModel{}
+	if _, err := rsp.ReadPaginationData(&os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	if len(os) == 0 {
+		t.SkipNow()
+	}
+
+	for _, o := range os {
+		switch {
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.TradeId == "":
+			t.Error("empty key 'TradeId'")
+		case o.Liability == "":
+			t.Error("empty key 'Liability'")
+		case o.Principal == "":
+			t.Error("empty key 'Principal'")
+		case o.AccruedInterest == "":
+			t.Error("empty key 'AccruedInterest'")
+		case o.CreatedAt == "":
+			t.Error("empty key 'createdAt'")
+		case o.MaturityTime == "":
+			t.Error("empty key 'MaturityTime'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.RepaidSize == "":
+			t.Error("empty key 'RepaidSize'")
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		}
+	}
+}
+
+func TestApiService_BorrowRepaidRecords(t *testing.T) {
+	s := NewApiServiceFromEnv()
+	pagination := &PaginationParam{
+		CurrentPage: 1,
+		PageSize:    10,
+	}
+
+	rsp, err := s.BorrowRepaidRecords("", pagination)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := BorrowRepaidRecordsModel{}
+	if _, err := rsp.ReadPaginationData(&os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	if len(os) == 0 {
+		t.SkipNow()
+	}
+
+	for _, o := range os {
+		switch {
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		case o.Interest == "":
+			t.Error("empty key 'Interest'")
+		case o.Principal == "":
+			t.Error("empty key 'Principal'")
+		case o.RepayTime == "":
+			t.Error("empty key 'RepayTime'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.TradeId == "":
+			t.Error("empty key 'TradeId'")
+		}
+	}
+}
+
+func TestApiService_RepayAll(t *testing.T) {
+	t.SkipNow()
+	s := NewApiServiceFromEnv()
+	params := map[string]string{"currency": "BTC", "sequence": "RECENTLY_EXPIRE_FIRST", "size": "0.004"}
+	rsp, err := s.RepayAll(params)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("RawData: %+v", string(rsp.RawData))
+}
+
+func TestApiService_RepaySingle(t *testing.T) {
+	t.SkipNow()
+	s := NewApiServiceFromEnv()
+	params := map[string]string{"currency": "BTC", "tradeId": "5e0dca1fdd28950009db2530", "size": "0.001"}
+	rsp, err := s.RepaySingle(params)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("RawData: %+v", string(rsp.RawData))
+}
+
+func TestApiService_CreateLendOrder(t *testing.T) {
+	t.SkipNow()
+	s := NewApiServiceFromEnv()
+	params := map[string]string{"currency": "BTC", "size": "0.02", "dailyIntRate": "0.002", "term": "28"}
+	rsp, err := s.CreateLendOrder(params)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	o := &CreateLendOrderResultModel{}
+	if err := rsp.ReadData(o); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(o))
+
+	switch {
+	case o.OrderId == "":
+		t.Error("empty key 'OrderId'")
+	}
+}
+
+func TestApiService_CancelLendOrder(t *testing.T) {
+	t.SkipNow()
+	s := NewApiServiceFromEnv()
+
+	rsp, err := s.CancelLendOrder("5e0ea4de6817f00008a313a9")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("RawData: %+v", string(rsp.RawData))
+}
+
+func TestApiService_ToggleAutoLend(t *testing.T) {
+	t.SkipNow()
+	s := NewApiServiceFromEnv()
+
+	params := map[string]string{"currency": "BTC", "isEnable": "true", "retainSize": "0.4", "dailyIntRate": "0.002", "term": "14"}
+	rsp, err := s.ToggleAutoLend(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("RawData: %+v", string(rsp.RawData))
+}
+
+func TestApiService_LendActiveOrders(t *testing.T) {
+	s := NewApiServiceFromEnv()
+	pagination := &PaginationParam{
+		CurrentPage: 1,
+		PageSize:    10,
+	}
+
+	rsp, err := s.LendActiveOrders("BTC", pagination)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := LendActiveOrdersModel{}
+	if _, err := rsp.ReadPaginationData(&os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	if len(os) == 0 {
+		t.SkipNow()
+	}
+
+	for _, o := range os {
+		switch {
+		case o.OrderId == "":
+			t.Error("empty key 'OrderId'")
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.Size == "":
+			t.Error("empty key 'Size'")
+		case o.FilledSize == "":
+			t.Error("empty key 'FilledSize'")
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.CreatedAt == "":
+			t.Error("empty key 'CreatedAt'")
+		}
+	}
+}
+
+func TestApiService_LendDoneOrders(t *testing.T) {
+	s := NewApiServiceFromEnv()
+	pagination := &PaginationParam{
+		CurrentPage: 1,
+		PageSize:    10,
+	}
+
+	rsp, err := s.LendDoneOrders("BTC", pagination)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := LendDoneOrdersModel{}
+	if _, err := rsp.ReadPaginationData(&os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	if len(os) == 0 {
+		t.SkipNow()
+	}
+
+	for _, o := range os {
+		switch {
+		case o.OrderId == "":
+			t.Error("empty key 'OrderId'")
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.Size == "":
+			t.Error("empty key 'Size'")
+		case o.FilledSize == "":
+			t.Error("empty key 'FilledSize'")
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.CreatedAt == "":
+			t.Error("empty key 'CreatedAt'")
+		case o.Status == "":
+			t.Error("empty key 'Status'")
+		}
+	}
+}
+
+func TestApiService_LendTradeUnsettledRecords(t *testing.T) {
+	s := NewApiServiceFromEnv()
+	pagination := &PaginationParam{
+		CurrentPage: 1,
+		PageSize:    10,
+	}
+
+	rsp, err := s.LendTradeUnsettledRecords("BTC", pagination)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := LendTradeUnsettledRecordsModel{}
+	if _, err := rsp.ReadPaginationData(&os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	if len(os) == 0 {
+		t.SkipNow()
+	}
+
+	for _, o := range os {
+		switch {
+		case o.TradeId == "":
+			t.Error("empty key 'TradeId'")
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.Size == "":
+			t.Error("empty key 'Size'")
+		case o.AccruedInterest == "":
+			t.Error("empty key 'AccruedInterest'")
+		case o.Repaid == "":
+			t.Error("empty key 'Repaid'")
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.MaturityTime == "":
+			t.Error("empty key 'MaturityTime'")
+		}
+	}
+}
+
+func TestApiService_LendTradeSettledRecords(t *testing.T) {
+	s := NewApiServiceFromEnv()
+	pagination := &PaginationParam{
+		CurrentPage: 1,
+		PageSize:    10,
+	}
+
+	rsp, err := s.LendTradeSettledRecords("BTC", pagination)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := LendTradeSettledRecordsModel{}
+	if _, err := rsp.ReadPaginationData(&os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	if len(os) == 0 {
+		t.SkipNow()
+	}
+
+	for _, o := range os {
+		switch {
+		case o.TradeId == "":
+			t.Error("empty key 'TradeId'")
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.Size == "":
+			t.Error("empty key 'Size'")
+		case o.Interest == "":
+			t.Error("empty key 'Interest'")
+		case o.Repaid == "":
+			t.Error("empty key 'Repaid'")
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.SettledAt == "":
+			t.Error("empty key 'SettledAt'")
+		case o.Note == "":
+			t.Error("empty key 'Note'")
+		}
+	}
+}
+
+func TestApiService_LendAssets(t *testing.T) {
+	s := NewApiServiceFromEnv()
+
+	rsp, err := s.LendAssets("BTC")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := &LendAssetsModel{}
+	if err := rsp.ReadData(os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	for _, o := range *os {
+		switch {
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.Outstanding == "":
+			t.Error("empty key 'Outstanding'")
+		case o.FilledSize == "":
+			t.Error("empty key 'FilledSize'")
+		case o.AccruedInterest == "":
+			t.Error("empty key 'AccruedInterest'")
+		case o.RealizedProfit == "":
+			t.Error("empty key 'RealizedProfit'")
+		}
+	}
+}
+
+func TestApiService_MarginMarkets(t *testing.T) {
+	s := NewApiServiceFromEnv()
+
+	params := map[string]string{"currency": "BTC"}
+	rsp, err := s.MarginMarkets(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := &MarginMarketsModel{}
+	if err := rsp.ReadData(os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	for _, o := range *os {
+		switch {
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.Size == "":
+			t.Error("empty key 'Size'")
+		}
+	}
+}
+
+func TestApiService_MarginTradeLast(t *testing.T) {
+	s := NewApiServiceFromEnv()
+
+	rsp, err := s.MarginTradeLast("BTC")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os := &MarginTradesModel{}
+	if err := rsp.ReadData(os); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(ToJsonString(os))
+
+	for _, o := range *os {
+		switch {
+		case o.TradeId == "":
+			t.Error("empty key 'TradeId'")
+		case o.Currency == "":
+			t.Error("empty key 'Currency'")
+		case o.Size == "":
+			t.Error("empty key 'Size'")
+		case o.DailyIntRate == "":
+			t.Error("empty key 'DailyIntRate'")
+		case o.Term == "":
+			t.Error("empty key 'Term'")
+		case o.Timestamp == "":
+			t.Error("empty key 'Timestamp'")
+		}
+	}
+}
